ded64571f99f07092fcd887b9d4e7e84a097a0ca
! Nirjal <81520082+Nirjal112233@users.noreply.github.com>
2022-08-12 09:39:12 +0545

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,6 @@
+# This Is A Rock Paper Scissors Program Code ByTech With Tim Offical Video  https://www.youtube.com/watch?v=DLn3jOsNRVE Go watch it if you want full credit to him
+#you can add questions if u want
+
 print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
@@ -8,28 +11,28 @@ if playing.lower() != "yes":
 print("Okay! Let's play :)")
 score = 0
 
-answer = input("What does CPU stand for? ")
+answer = input("What does CPU stand for? ").lower
 if answer.lower() == "central processing unit":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does GPU stand for? ")
+answer = input("What does GPU stand for? ").lower
 if answer.lower() == "graphics processing unit":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does RAM stand for? ")
+answer = input("What does RAM stand for? ").lower
 if answer.lower() == "random access memory":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does PSU stand for? ")
+answer = input("What does PSU stand for? ").lower
 if answer.lower() == "power supply":
     print('Correct!')
     score += 1
